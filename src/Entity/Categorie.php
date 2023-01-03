fda26574e097cb030a5f004d1a6b88ca9ad2f4e6
mayados <mariannedos.cours@gmail.com>
2023-01-03 16:44:04 +0100

    add modules

--- src/Entity/Categorie.php
+++ src/Entity/Categorie.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\CategorieRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CategorieRepository::class)]
@@ -15,6 +17,14 @@ class Categorie
 
     #[ORM\Column(length: 255)]
     private ?string $intitule = null;
+
+    #[ORM\OneToMany(mappedBy: 'categorie', targetEntity: Module::class, orphanRemoval: true)]
+    private Collection $modules;
+
+    public function __construct()
+    {
+        $this->modules = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -29,6 +39,36 @@ class Categorie
     public function setIntitule(string $intitule): self
     {
         $this->intitule = $intitule;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Module>
+     */
+    public function getModules(): Collection
+    {
+        return $this->modules;
+    }
+
+    public function addModule(Module $module): self
+    {
+        if (!$this->modules->contains($module)) {
+            $this->modules->add($module);
+            $module->setCategorie($this);
+        }
+
+        return $this;
+    }
+
+    public function removeModule(Module $module): self
+    {
+        if ($this->modules->removeElement($module)) {
+            // set the owning side to null (unless already changed)
+            if ($module->getCategorie() === $this) {
+                $module->setCategorie(null);
+            }
+        }
 
         return $this;
     }
