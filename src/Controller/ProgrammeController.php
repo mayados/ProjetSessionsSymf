9621d851cc8840e6c4d90e4a048eb6a0c318c846
mayados <115099504+mayados@users.noreply.github.com>
2023-01-05 17:52:29 +0100

    create public function add programme

--- src/Controller/ProgrammeController.php
+++ src/Controller/ProgrammeController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Programme;
+use App\Form\ProgrammeType;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProgrammeController extends AbstractController
 {
@@ -15,4 +19,26 @@ class ProgrammeController extends AbstractController
             'controller_name' => 'ProgrammeController',
         ]);
     }
+
+    #[Route('/programme/add', name: 'add_programme')]
+    public function add(ManagerRegistry $doctrine, Programme $programme = null, Request $request): Response
+    {
+
+        $form = $this->createForm(ProgrammeType::class, $programme);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $programme = $form->getData();
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($programme);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_session');
+        }
+
+        return $this->render('programme/add.html.twig', [
+            'formAddProgramme' => $form->createView() 
+        ]);
+    }
+
 }
