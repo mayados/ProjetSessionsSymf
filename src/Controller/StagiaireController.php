70fe5982f13f5ac8b6157eab8fed1dc0fb96b0b6
mayados <115099504+mayados@users.noreply.github.com>
2023-01-05 12:04:38 +0100

    add code to function index() / add function show()

--- src/Controller/StagiaireController.php
+++ src/Controller/StagiaireController.php
@@ -2,17 +2,34 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Stagiaire;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class StagiaireController extends AbstractController
 {
-    #[Route('/stagiaire', name: 'app_stagiaire')]
-    public function index(): Response
+
+    #[Route('/stagiaire/{id}', name: 'show_stagiaire')]
+    public function show(Stagiaire $stagiaire): Response
     {
+        return $this->render('stagiaire/show.html.twig', [
+            'stagiaire' => $stagiaire,
+        ]);
+    }    
+
+    #[Route('/stagiaires', name: 'app_stagiaire')]
+    public function index(ManagerRegistry $doctrine): Response
+    {
+
+        $stagiaires = $doctrine->getRepository(Stagiaire::class)->findAll();
+
         return $this->render('stagiaire/index.html.twig', [
-            'controller_name' => 'StagiaireController',
+            'stagiaires' => $stagiaires,
         ]);
     }
+
+
+
 }
