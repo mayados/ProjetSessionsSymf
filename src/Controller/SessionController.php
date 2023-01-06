ba9d841a344040c6aa3b7c53e5f9c92dc8c98e53
mayados <115099504+mayados@users.noreply.github.com>
2023-01-06 15:45:07 +0100

    create function removeStagiaire()

--- src/Controller/SessionController.php
+++ src/Controller/SessionController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Session;
 use App\Entity\Programme;
+use App\Entity\Stagiaire;
 use App\Repository\SessionRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
@@ -24,6 +25,26 @@ class SessionController extends AbstractController
             'futureSessions' => $futureSessions,
             'progressSessions' => $progressSessions,
         ]);
+    }
+
+    #[Route('/session/removeStagiaire/{id}/{idStagiaire}', name: 'remove_stagiaire')]
+    public function removeStagiaire(ManagerRegistry $doctrine , Session $session, Stagiaire $stagiaire, int $idStagiaire): Response
+    {
+
+        /* Des choses vont être changées en base de données session_stagiaire, il faut donc doctrine */
+        $entityManager = $doctrine->getManager();
+        /* On cherche de quelle instance de class il s'agit grâce à l'id du stagiaire envoyée en url */
+        $stagiaire = $entityManager->getRepository(Stagiaire::class)->find($idStagiaire);        
+        /* On appelle une méthode de la Class, pas une méthode du repository */
+        /* On dit que le stagiaire concerné est celui que la méthode find() renvoie  */
+        $session->removeStagiaire($stagiaire);
+        /* flush() sauvegarde les changements effectués en base de données */
+        $entityManager->flush();
+
+
+        //Voir comment rediriger sur une vue précise. Par exemple la session où l'on était
+        return $this->redirectToRoute('app_session');
+
     }
 
     #[Route('/session/{id}', name: 'show_session')]
