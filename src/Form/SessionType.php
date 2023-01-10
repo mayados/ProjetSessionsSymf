c5d4f0659093459952dbf2f2ec8f318e840c559d
tamim <mariannedos.cours@gmail.com>
2023-01-10 11:36:48 +0100

    creation form stagiaire / session

---
+++ src/Form/SessionType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Session;
+use App\Entity\Formateur;
+use App\Entity\Formation;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+
+class SessionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('intitule', TextType::class)
+            ->add('dateDebut', DateType::class)
+            ->add('dateFin', DateType::class)
+            ->add('nbPlaces', IntegerType::class)
+            ->add('referent', EntityType::class, [
+                'class' => Formateur::class,
+                'placeholder' => '...',
+                'required' => false,
+            ])
+            ->add('formation', EntityType::class, [
+                'class' => Formation::class,
+            ])
+            ->add('submit', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Session::class,
+        ]);
+    }
+}
---
+++ src/Form/StagiaireFormType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Stagiaire;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+
+class StagiaireFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom', TextType::class)
+            ->add('prenom', TextType::class)
+            ->add('sexe', ChoiceType::class, [
+                'choices' => [
+                    'Homme' => "homme",
+                    'femme' => "femme",
+                ],
+
+            ])
+            ->add('dateNaissance', DateType::class)
+            ->add('ville', TextType::class)
+            ->add('mail', TextType::class)
+            ->add('telephone', IntegerType::class)
+            ->add('submit', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Stagiaire::class,
+        ]);
+    }
+}
