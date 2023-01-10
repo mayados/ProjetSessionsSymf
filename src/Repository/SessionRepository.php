947edd7bd4680b29de9da8cc571836f94f285568
tamim <mariannedos.cours@gmail.com>
2023-01-10 15:43:17 +0100

    create function findNonInscrits()

--- src/Repository/SessionRepository.php
+++ src/Repository/SessionRepository.php
@@ -109,5 +109,46 @@ class SessionRepository extends ServiceEntityRepository
 
     }
 
+    public function findNonInscrits($id)
+    {
+        //em sert à acceder à doctrine
+        $em = $this->getEntityManager();
+        //Sert à créér une requête DQL (déjà lié à doctrine de par $em)
+        $sub = $em->createQueryBuilder();
+
+        //Requête en deux temps
+
+        // $qb est égal à la création d'une requête + la connexion à Doctrine
+        $qb = $sub;
+        /*Sélectionner tous les stagiaires d'une session dont l'id est passé en paramètre*/
+        //On séléctionne l'objet d'allias 's' avec toutes ses propriétés
+        $qb->select('s')
+        ->from('App\Entity\Stagiaire', 's')
+        // On fait un leftJoin sur la collection sessions de l'entité Stagiaire (car la relation ManyToMany joint Sessions et Stagiaires)
+        ->leftJoin('s.sessions', 'se')
+        // Où l'id de la session est égal à l'id entré en paramètres
+        ->where('se.id = :id');
+
+        // On doit redéfinir sub car ici on fait une autre requête, qui est la suite de la requête
+        $sub = $em->createQueryBuilder();
+        /* Sélectionner tous les stagiaires qui ne sont pas (NOT IN) le résultat précédent
+        => On obtient les stagiaires non inscrits pour une session définie */
+        $sub->select('st')
+        // On donne l'allias st à l'entité Stagiaire
+        ->from('App\Entity\Stagiaire', 'st')
+        // Où expr() est un expressionBuilder (sert à utiliser les conditions comme notIn)  les stagiaires dont l'id n'est pas dans la requête précédente 
+        ->where($sub->expr()->notIn('st.id', $qb->getDQL()))
+        // Requête préparée -> on protège contre l'injection SQL
+        ->setParameter('id', $id)
+        //On trie sur le nom
+        ->orderBy('st.nom');
+
+        //renvoyer le résultat
+        //On attribue à $query les valeurs de sub
+        $query = $sub->getQuery();
+        return $query->getResult();
+
+    }
+
 
 }
