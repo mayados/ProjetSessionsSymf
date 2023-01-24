bd0da434c2693ef627b88db4ad7aa01ddcb2c348
mayados <mariannedos.cours@gmail.com>
2023-01-24 09:58:52 +0100

    cange length property mail

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -17,7 +17,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 180, unique: true)]
+    #[ORM\Column(length: 200, unique: true)]
     private ?string $email = null;
 
     #[ORM\Column]
