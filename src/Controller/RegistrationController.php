8e70619e1b676ff34d31776446345409b8fb81b1
mayados <mariannedos.cours@gmail.com>
2023-01-25 16:56:01 +0100

    change subject of mail

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -51,7 +51,7 @@ class RegistrationController extends AbstractController
                 (new TemplatedEmail())
                     ->from(new Address('admin@session.com', 'Admin SymfSessions'))
                     ->to($user->getEmail())
-                    ->subject('Please Confirm your Email')
+                    ->subject('Veuillez confirmer votre mail')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
             );
             // do anything else you need here, like send an email
@@ -83,7 +83,7 @@ class RegistrationController extends AbstractController
         }
 
         // @TODO Change the redirect on success and handle or remove the flash message in your templates
-        $this->addFlash('success', 'Your email address has been verified.');
+        $this->addFlash('success', 'Votre adresse mail a été vérifiée');
 
         return $this->redirectToRoute('app_register');
     }
